25245e126995740c5174515025a64b20aa463f11
Tahjour <tahjallman64@gmail.com>
2023-09-18 01:11:39 -0400

    added mongodb driver for winston logger

--- lib/database/mongodb.ts
+++ lib/database/mongodb.ts
@@ -1,15 +1,21 @@
 // lib\database\mongodb.ts
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.nrje3.mongodb.net/?retryWrites=true&w=majority`;
-let mongoClient = new MongoClient(uri);
+export const MONGODB_URI = process.env.MONGODB_URI!;
+let mongoClient = new MongoClient(MONGODB_URI);
 let cachedClient: MongoClient | null = null;
 export async function getConnectedClient() {
   if (cachedClient) {
     return cachedClient;
   }
 
-  mongoClient = new MongoClient(uri);
+  mongoClient = new MongoClient(MONGODB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
   cachedClient = await mongoClient.connect();
   return cachedClient;
 }
--- lib/helpers/logger.ts
+++ lib/helpers/logger.ts
@@ -1,5 +1,6 @@
 // lib\helpers\logger.ts
 import { createLogger, format, transports } from "winston";
+import "winston-mongodb";
 const { timestamp, errors, combine, prettyPrint } = format;
 
 export const logger = createLogger({
@@ -18,8 +19,15 @@ export const logger = createLogger({
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new transports.File({ filename: "error.log", level: "error" }),
-    new transports.File({ filename: "combined.log" }),
+    // new transports.File({ filename: "error.log", level: "error" }),
+    // new transports.File({ filename: "combined.log" }),
+    new transports.MongoDB({
+      db: process.env.MONGODB_URI!,
+      collection: "logs",
+      options: {
+        useUnifiedTopology: true,
+      },
+    }),
   ],
 });
 
